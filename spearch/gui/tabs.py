0c8be003d1f41f4dc17fee1055609b0bb587b2da
Jacob Scott <jscott12009@gmail.com>
2018-03-20 23:04:41 -0400

    Revert "Create add_songs method for custom song table"
    This reverts commit 5cc10d8ccf8091a35ead54d3c28a5ef58f073d57.

--- spearch/gui/tabs.py
+++ spearch/gui/tabs.py
@@ -21,7 +21,7 @@ class SongUI(QWidget):
 
         # Display the songs of a playlist that is selected from the combo box
         self.playlists.currentIndexChanged.connect(
-            lambda: self.add_songs(
+            lambda: self.list_songs(
                 self.user.pl_ids[self.playlists.currentIndex()]))
 
     def init_ui(self):
@@ -31,30 +31,126 @@ class SongUI(QWidget):
         self.playlists.addItems(self.user.playlists)
 
         # Create button to add songs to queue
-        self.add_songs_button = QPushButton('Add', self)
-        self.add_songs_button.setMaximumWidth(1.2 * self.add_songs_button.width())
+        self.add_songs = QPushButton('Add', self)
+        self.add_songs.setMaximumWidth(1.2 * self.add_songs.width())
 
         # Create song list
-        self.songs_table = SongArtistTableWidget(self, select_artists=False)
+        self.songs_table = SongArtistTableWidget(self)
 
         # Put it all together
         top_row = QHBoxLayout()
-        top_row.addWidget(self.add_songs_button)
+        top_row.addWidget(self.add_songs)
         top_row.addWidget(self.playlists)
         v_box = QVBoxLayout(self)
         v_box.addLayout(top_row)
         v_box.addWidget(self.songs_table)
 
-    def add_songs(self, pl_id):
+    def list_songs(self, pl_id):
         """
-        Gets the songs, from the currently selected playlist and adds them to
-        the table
+        Adds the songs and their artists from a playlist to the Table Widget
 
         Parameters:
         pl_id - The ID of the playlist
         """
+        # Get the song data
         songs = self.user.get_playlist_songs(pl_id)
-        self.songs_table.add_songs(songs)
+
+        # Reset the data by removing old songs and whatnot
+        for _ in range(self.songs_table.rowCount()):
+            self.songs_table.removeRow(0)
+        self.songs_table.setSortingEnabled(False)
+
+        for n, song in enumerate(songs):
+            # Create the row
+            self.songs_table.insertRow(n)
+
+            # Add song name
+            self.songs_table.setItem(n, 0, QTableWidgetItem(song[0]))
+
+            # Add artist(s)
+            artist = QTableWidgetItem(', '.join(song[1]))
+            # Remove the ability to select it
+            artist.setFlags(QtCore.Qt.ItemIsEnabled)
+            artist.setFlags(QtCore.Qt.ItemIsSelectable)
+            self.songs_table.setItem(n, 1, artist)
+
+            # Hidden column with song IDs
+            self.songs_table.setItem(n, 2, QTableWidgetItem(song[2]))
+
+        self.songs_table.setSortingEnabled(True)
+
+
+class CreateQueueUI(QWidget):
+
+    def __init__(self, parent, user):
+        """
+        Widget/tab that allows the creation of a queue given some logic
+        """
+        super().__init__(parent)
+        self.user = user
+        self.init_ui()
+
+        # Clear the queue list
+        self.clear_songs.clicked.connect(self.clear_queue)
+
+        # Make playlist
+        self.create_playlist.clicked.connect(self.make_new_playlist)
+
+    def init_ui(self):
+        # Button to add songs to queue
+        self.create_songs = QPushButton('Create', self)
+        # Button to clear songs from queue
+        self.clear_songs = QPushButton('Clear', self)
+        # Button to save current songs to a new playlist
+        self.create_playlist = QPushButton('Make Playlist', self)
+
+        # List of songs and their artists
+        self.queue_list = SongArtistTableWidget(self)
+
+        # Put it all together
+        v_box = QVBoxLayout()
+        v_box.addStretch()
+        v_box.addWidget(self.create_playlist)
+        v_box.addWidget(self.clear_songs)
+        v_box.addWidget(self.create_songs)
+
+        h_box = QHBoxLayout(self)
+        h_box.addLayout(v_box)
+        h_box.addWidget(self.queue_list)
+
+    def clear_queue(self):
+        """
+        Removes all of the songs from the queue list maker
+        """
+        # Reset the data by removing old songs and whatnot
+        for _ in range(self.queue_list.rowCount()):
+            self.queue_list.removeRow(0)
+
+    def make_new_playlist(self):
+        """
+        Creates a new playlist from the current listed songs
+        """
+        playlist_name, private, ok = NewPlaylistDialog.getPlaylistName()
+
+        if ok:
+            song_ids = [self.queue_list.item(row, 2).text() for row in
+                            range(self.queue_list.rowCount())]
+            self.user.create_playlist(song_ids, playlist_name, not private)
+
+
+class CurrentQueueUI(QWidget):
+
+    def __init__(self, parent, user):
+        super().__init__(parent)
+        self.user = user
+        self.init_ui()
+
+    def init_ui(self):
+        # List of songs and their artists
+        self.current_queue = SongArtistTableWidget(self)
+
+        v_box = QVBoxLayout(self)
+        v_box.addWidget(self.current_queue)
 
 
 class FilterUI(QWidget):
@@ -72,22 +168,16 @@ class FilterUI(QWidget):
         self.add_pl.clicked.connect(self.add_playlist)
         # Button to show what songs are return by total filter
         self.commit = QPushButton('Commit', self)
-        # Create button to add songs to queue
-        self.add_songs_button = QPushButton('Add', self)
-        self.add_songs_button.setMaximumWidth(1.2 * self.add_songs_button.width())
         # Table to show filtered songs
-        self.songs_table = SongArtistTableWidget(self, False, False, False)
+        self.songs_table = SongArtistTableWidget(self)
 
         # Total layout for playlist filters
         self.filt_layout = QGridLayout()
         self.filt_layout.addWidget(self.add_pl, 1000, 0)
 
         # Put it all together
-        h_box_buttons = QHBoxLayout()
-        h_box_buttons.addWidget(self.add_songs_button)
-        h_box_buttons.addWidget(self.commit)
         v_box = QVBoxLayout()
-        v_box.addLayout(h_box_buttons)
+        v_box.addWidget(self.commit)
         v_box.addWidget(self.songs_table)
         h_box = QHBoxLayout(self)
         h_box.addLayout(self.filt_layout)
@@ -105,17 +195,16 @@ class FilterUI(QWidget):
         # Clean the data up to pass to self.user.filter_playlist
         filt_dict = self.sanitize_data(filt_dict)
 
-        filt_songs = []
-        # For each playlist, get the filtered song list
         for pl, pl_data in filt_dict.items():
             songs = self.user.get_playlist_songs(
                 self.user.pl_ids[self.user.playlists.index(pl)])
-            filt_songs += self.user.filter_playlist(songs, _or=pl_data)
+            filt_songs = self.user.filter_playlist(songs, _or=pl_data)
 
-        self.songs_table.add_songs(filt_songs)
+            self.list_songs(filt_songs)
+
 
     def parse_filt_layout(self, layout=None, depth=0, filt_dict=None, d_ind=0,
-                          latest_pl=None, latest_logic=None, latest_filt=None):
+                         latest_pl=None, latest_logic=None, latest_filt=None):
         """
         Recursively queries the data from this tab to filt the playlist(s)
         selected. The arguments are used for the recursive-ness.
@@ -154,18 +243,25 @@ class FilterUI(QWidget):
             if isinstance(item, QWidgetItem):
                 # and a Combobox...
                 if isinstance(item.widget(), QComboBox):
-                    item_key = item.widget().currentText() + '&&' + str(d_ind)
-                    d_ind += 1
                     # A recursive depth of 1 is the playlist name
                     if depth == 1:
+                        item_key = item.widget().currentText() + \
+                                   '&&' + str(d_ind)
+                        d_ind += 1
                         filt_dict[item_key] = {}
                         latest_pl = item_key
                     # A recursive depth of 2 is the logic name
                     if depth == 2:
+                        item_key = item.widget().currentText() + \
+                                   '&&' + str(d_ind)
+                        d_ind += 1
                         filt_dict[latest_pl][item_key] = {}
                         latest_logic = item_key
                     # A recursive depth of 3 is the filter type
                     if depth == 3:
+                        item_key = item.widget().currentText() + \
+                                   '&&' + str(d_ind)
+                        d_ind += 1
                         filt_dict[latest_pl][latest_logic][item_key] = ''
                         latest_filt = item_key
                 # and a LineEdit
@@ -207,7 +303,7 @@ class FilterUI(QWidget):
                     # For each AND, append dict to the AND list and same for OR
                     clean_dict[clean_pl][clean_logic][-1][clean_filt] = filt_data
 
-        return clean_dict
+        return dict(clean_dict)
 
     def add_playlist(self):
         """
@@ -291,6 +387,10 @@ class FilterUI(QWidget):
             self.remove_widgets(new_filter, new_filter_remove, new_filter_input))
 
         logic_layout.addLayout(new_filter_grid, logic_layout.count(), 0)
+        # # Add widgets to the child layout
+        # logic_layout.addWidget(new_filter, logic_layout.count(), 0)
+        # logic_layout.addWidget(new_filter_remove, logic_layout.count() - 1, 1)
+        # logic_layout.addWidget(new_filter_input, logic_layout.count() - 1, 0)
 
     def remove_layout(self, layout):
         """
@@ -316,106 +416,34 @@ class FilterUI(QWidget):
         for widget in widgets:
             widget.deleteLater()
 
-    # def list_songs(self, songs):
-    #     """
-    #     Adds the songs and their artists from a playlist to the Table Widget
-
-    #     Parameters:
-    #     songs - The songs to display
-    #     """
-    #     # Reset the data by removing old songs and whatnot
-    #     for _ in range(self.songs_table.rowCount()):
-    #         self.songs_table.removeRow(0)
-    #     self.songs_table.setSortingEnabled(False)
-
-    #     for n, song in enumerate(songs):
-    #         # Create the row
-    #         self.songs_table.insertRow(n)
-
-    #         # Add song name
-    #         self.songs_table.setItem(n, 0, QTableWidgetItem(song[0]))
-
-    #         # Add artist(s)
-    #         artist = QTableWidgetItem(', '.join(song[1]))
-    #         # Remove the ability to select it
-    #         artist.setFlags(QtCore.Qt.ItemIsEnabled)
-    #         artist.setFlags(QtCore.Qt.ItemIsSelectable)
-    #         self.songs_table.setItem(n, 1, artist)
-
-    #         # Hidden column with song IDs
-    #         self.songs_table.setItem(n, 2, QTableWidgetItem(song[2]))
-
-    #     self.songs_table.setSortingEnabled(True)
-
-
-class CreateQueueUI(QWidget):
-
-    def __init__(self, parent, user):
+    def list_songs(self, songs):
         """
-        Widget/tab that allows the creation of a queue given some logic
+        Adds the songs and their artists from a playlist to the Table Widget
+
+        Parameters:
+        songs - The songs to display
         """
-        super().__init__(parent)
-        self.user = user
-        self.init_ui()
 
-        # Clear the queue list
-        self.clear_songs.clicked.connect(self.clear_queue)
-
-        # Make playlist
-        self.create_playlist.clicked.connect(self.make_new_playlist)
-
-    def init_ui(self):
-        # Button to add songs to queue
-        self.create_songs = QPushButton('Create', self)
-        # Button to clear songs from queue
-        self.clear_songs = QPushButton('Clear', self)
-        # Button to save current songs to a new playlist
-        self.create_playlist = QPushButton('Make Playlist', self)
-
-        # List of songs and their artists
-        self.queue_list = SongArtistTableWidget(self)
-
-        # Put it all together
-        v_box = QVBoxLayout()
-        v_box.addStretch()
-        v_box.addWidget(self.create_playlist)
-        v_box.addWidget(self.clear_songs)
-        v_box.addWidget(self.create_songs)
-
-        h_box = QHBoxLayout(self)
-        h_box.addLayout(v_box)
-        h_box.addWidget(self.queue_list)
-
-    def clear_queue(self):
-        """
-        Removes all of the songs from the queue list maker
-        """
         # Reset the data by removing old songs and whatnot
-        for _ in range(self.queue_list.rowCount()):
-            self.queue_list.removeRow(0)
+        for _ in range(self.songs_table.rowCount()):
+            self.songs_table.removeRow(0)
+        self.songs_table.setSortingEnabled(False)
 
-    def make_new_playlist(self):
-        """
-        Creates a new playlist from the current listed songs
-        """
-        playlist_name, private, ok = NewPlaylistDialog.getPlaylistName()
+        for n, song in enumerate(songs):
+            # Create the row
+            self.songs_table.insertRow(n)
 
-        if ok:
-            song_ids = [self.queue_list.item(row, 2).text() for row in
-                            range(self.queue_list.rowCount())]
-            self.user.create_playlist(song_ids, playlist_name, not private)
+            # Add song name
+            self.songs_table.setItem(n, 0, QTableWidgetItem(song[0]))
 
+            # Add artist(s)
+            artist = QTableWidgetItem(', '.join(song[1]))
+            # Remove the ability to select it
+            artist.setFlags(QtCore.Qt.ItemIsEnabled)
+            artist.setFlags(QtCore.Qt.ItemIsSelectable)
+            self.songs_table.setItem(n, 1, artist)
 
-class CurrentQueueUI(QWidget):
+            # Hidden column with song IDs
+            self.songs_table.setItem(n, 2, QTableWidgetItem(song[2]))
 
-    def __init__(self, parent, user):
-        super().__init__(parent)
-        self.user = user
-        self.init_ui()
-
-    def init_ui(self):
-        # List of songs and their artists
-        self.current_queue = SongArtistTableWidget(self)
-
-        v_box = QVBoxLayout(self)
-        v_box.addWidget(self.current_queue)
+        self.songs_table.setSortingEnabled(True)
